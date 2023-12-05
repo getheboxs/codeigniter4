283ba78ddfdaaa1d3fa6e46b47cfe084da14a001
Zakki Alawi <alw.zakki@gmail.com>
2023-12-05 10:11:23 +0700

    database

---
+++ unugiri_2023_codeigniter4.sql
@@ -0,0 +1,159 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 05, 2023 at 03:10 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `unugiri_2023_codeigniter4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `kd_kategori` varchar(4) NOT NULL,
+  `nama_kategori` varchar(50) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penerbit`
+--
+
+CREATE TABLE `penerbit` (
+  `id_penerbit` int(11) NOT NULL,
+  `nm_penerbit` varchar(50) NOT NULL,
+  `kt_penerbit` varchar(50) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `penerbit`
+--
+
+INSERT INTO `penerbit` (`id_penerbit`, `nm_penerbit`, `kt_penerbit`, `keterangan`) VALUES
+(1, 'Informatika', 'Bandung', '-'),
+(2, 'Andi', 'Yogyakarta', '-');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_penerbit`
+--
+
+CREATE TABLE `tb_penerbit` (
+  `id_penerbit` int(4) NOT NULL,
+  `nm_penerbit` varchar(100) NOT NULL,
+  `kt_penerbit` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_penerbit`
+--
+
+INSERT INTO `tb_penerbit` (`id_penerbit`, `nm_penerbit`, `kt_penerbit`) VALUES
+(1, 'Andi', 'Yogyakarta'),
+(2, 'Informatika', 'Bandung'),
+(3, 'Andi', 'Jakarta'),
+(4, 'Andi', 'Surabaya'),
+(5, 'Andi', 'Malang'),
+(6, 'Andi', 'Solo'),
+(7, 'Andi', 'Bojonegoro'),
+(8, 'Andi', 'Blora'),
+(9, 'Andi', 'Tuban'),
+(10, 'Andi', 'Lamongan'),
+(11, 'Andi', 'Tuban'),
+(12, 'Andi', 'Lamongan'),
+(13, 'Informatika', 'Jakarta'),
+(14, 'Informatika', 'Malang'),
+(15, 'Informatika', 'Malang'),
+(16, 'Informatika', 'Surabaya'),
+(17, 'Informatika', 'Bojonegoro'),
+(18, 'Informatika', 'Tuban'),
+(19, 'Informatika', 'Lamongan'),
+(20, 'Lokomedia', 'Jakarta'),
+(21, 'Lokomedia', 'bandung'),
+(22, 'aaa', 'aaa'),
+(24, 'informatika', 'malang');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_user`, `username`, `password`) VALUES
+(1, 'admin', '$2y$10$/clmNLUUxGnREFHqraU7P.aVfJ5Mk0iEKRgKVz8.ZKOZIUGJGXlya');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `penerbit`
+--
+ALTER TABLE `penerbit`
+  ADD PRIMARY KEY (`id_penerbit`) USING BTREE;
+
+--
+-- Indexes for table `tb_penerbit`
+--
+ALTER TABLE `tb_penerbit`
+  ADD PRIMARY KEY (`id_penerbit`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_penerbit`
+--
+ALTER TABLE `tb_penerbit`
+  MODIFY `id_penerbit` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
